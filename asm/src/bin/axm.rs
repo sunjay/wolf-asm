31ac9b4e21ec023f1cbac500302ebfc8d521d088
Sunjay Varma <varma.sunjay@gmail.com>
2020-06-05 14:36:20 -0600

    Created a basic executable that runs the assembly parser

--- asm/src/bin/axm.rs
+++ asm/src/bin/axm.rs
@@ -2,5 +2,53 @@
 //!
 //! Takes assembly code and generates machine code for the ax virtual machine
 
+use std::process;
+use std::sync::Arc;
+use std::path::Path;
+
+use parking_lot::RwLock;
+use termcolor::ColorChoice;
+
+use asm::{
+    parser::{self, SourceFiles},
+    diagnostics::Diagnostics,
+};
+
+macro_rules! quit {
+    ($diag:expr, $($args:tt)*) => {
+        {
+            $diag.error(format!($($args)*)).emit();
+            process::exit(1);
+        }
+    };
+}
+
+macro_rules! check_errors {
+    ($diag:expr) => {
+        let diag = $diag;
+        match diag.emitted_errors() {
+            0 => {},
+            1 => quit!(diag, "aborting due to 1 previous error"),
+            errors => quit!(diag, "aborting due to {} previous errors", errors),
+        }
+    };
+}
+
 fn main() {
+    //TODO: Parse command line arguments
+    let program_path = Path::new("asm/tests/ui/syntax.ax");
+    let color = ColorChoice::Auto;
+
+    let source_files = Arc::new(RwLock::new(SourceFiles::default()));
+    let diag = Diagnostics::new(source_files.clone(), color.into());
+
+    let root_file = source_files.write().add_file(program_path)
+        .unwrap_or_else(|err| quit!(&diag, "Could not read source file `{}`: {}", program_path.display(), err));
+    let program = {
+        // New scope because we want to drop this lock guard as soon as possible
+        let files = source_files.read();
+        parser::parse_program(files.source(root_file), &diag)
+    };
+    println!("{:#?}", program);
+    check_errors!(&diag);
 }
