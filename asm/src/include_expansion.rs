72587c9fecb784f0af09fdec4aecfed79e43d306
Sunjay Varma <varma.sunjay@gmail.com>
2020-06-06 19:38:57 -0600

    Updated expand_includes to be recursive so we can pass along context to be used for resolving paths

--- asm/src/include_expansion.rs
+++ asm/src/include_expansion.rs
@@ -1,6 +1,6 @@
 use std::str;
-use std::mem;
 use std::sync::Arc;
+use std::path::Path;
 
 use parking_lot::RwLock;
 
@@ -10,10 +10,10 @@ use crate::diagnostics::Diagnostics;
 
 /// Attempts to expand all `.include` directives in a program
 ///
-/// Up to `max_attempts` passes will be made. If all `.include` directives have not been resolved
-/// by then, the program is assumed to not have any fixed point and an error will be produced.
+/// Recurses up to `depth` times, after which an error will be produced if all `.include`
+/// directives have not been resolved.
 ///
-/// If an error occurs while processing an `.include` or while parsing any of the included files,
+/// If an error occurs while reading an included path or while parsing any of the included files,
 /// the errors will be outputted and the program will be returned in its current state.
 ///
 /// If no errors occur, the returned program is guaranteed to not have any remaining `.include`
@@ -22,81 +22,82 @@ pub fn expand_includes(
     prog: ast::Program,
     source_files: &Arc<RwLock<SourceFiles>>,
     diag: &Diagnostics,
-    max_attempts: usize,
+    depth: usize,
 ) -> ast::Program {
-    let ast::Program {mut stmts} = prog;
+    // This avoids a lot of unnecessary copying in exchange for an extra pass over the statements
+    let has_includes = prog.stmts.iter().any(|stmt| stmt.is_include());
+    if !has_includes {
+        return prog;
+    }
+
+    if depth == 0 {
+        diag.error("surpassed maximum include expansion attempts").emit();
+        return prog;
+    }
+
+    let ast::Program {stmts} = prog;
     // We will have at least the current amount of statements by the end of this
     let mut expanded_stmts = Vec::with_capacity(stmts.len());
 
-    for _ in 0..max_attempts {
-        // Avoids a lot of unnecessary copying in exchange for an extra pass over the statements
-        let has_includes = stmts.iter().any(|stmt| stmt.is_include());
-        if !has_includes {
-            return ast::Program {stmts};
-        }
+    for stmt in stmts {
+        let ast::Include {path: included_path} = match stmt {
+            ast::Stmt::Include(include) => include,
+            stmt => {
+                expanded_stmts.push(stmt);
+                continue;
+            },
+        };
 
-        // Use drain() so we can potentially reuse the allocation on the next pass
-        for stmt in stmts.drain(..) {
-            let ast::Include {path} = match stmt {
-                ast::Stmt::Include(include) => include,
-                stmt => {
-                    expanded_stmts.push(stmt);
-                    continue;
-                },
-            };
+        let path_span = included_path.span;
+        let included_path = match str::from_utf8(&included_path.value) {
+            Ok(path) => Path::new(path),
+            Err(err) => {
+                diag.span_error(included_path.span, format!("included path was not valid UTF-8: {}", err)).emit();
 
-            let path_span = path.span;
-            let path = match str::from_utf8(&path.value) {
-                Ok(path) => path,
-                Err(err) => {
-                    diag.span_error(path.span, format!("included path was not valid UTF-8: {}", err)).emit();
-
-                    // Finish this pass before stopping in case there are further errors
-                    continue;
-                },
-            };
-
-            // Note that we don't validate the extension of included files since that can be anything
-
-            //TODO: Resolve path relative to currently processed file
-
-            // Need this separate variable so that the lock on source files ends before diag.span_error()
-            let included_file = source_files.write().add_file(&*path);
-            let included_file = match included_file {
-                Ok(file_handle) => file_handle,
-                Err(err) => {
-                    diag.span_error(path_span, format!("unable to read included source file: `{}`: {}", path, err)).emit();
-                    // Finish this pass before stopping in case there are further errors
-                    continue;
-                },
-            };
-
-            let tokens = collect_tokens(source_files.read().source(included_file), diag);
-            if diag.emitted_errors() > 0 {
                 // Finish this pass before stopping in case there are further errors
                 continue;
-            }
+            },
+        };
 
-            let ast::Program {stmts: included_stmts} = parse_program(&tokens, diag);
-            if diag.emitted_errors() > 0 {
+        // Note that we don't validate the extension of included files since that can be anything
+
+        //TODO: Resolve path relative to currently processed file
+
+        // Need this separate variable so that the lock on source files ends before diag.span_error()
+        let included_file = source_files.write().add_file(included_path);
+        let included_file = match included_file {
+            Ok(file_handle) => file_handle,
+            Err(err) => {
+                diag.span_error(path_span, format!("unable to read included source file: `{}`: {}", included_path.display(), err)).emit();
                 // Finish this pass before stopping in case there are further errors
                 continue;
-            }
+            },
+        };
 
-            expanded_stmts.extend(included_stmts);
-        }
-
-        // For the next pass, iterate over the expanded statements and reuse the stmts allocation
-        // for the next set of expanded statements
-        mem::swap(&mut stmts, &mut expanded_stmts);
-
-        // Quit if this pass failed
+        let tokens = collect_tokens(source_files.read().source(included_file), diag);
         if diag.emitted_errors() > 0 {
-            return ast::Program {stmts};
+            // Finish this pass before stopping in case there are further errors
+            continue;
         }
+
+        let included_prog = parse_program(&tokens, diag);
+        if diag.emitted_errors() > 0 {
+            // Finish this pass before stopping in case there are further errors
+            continue;
+        }
+
+        // Recurse and expand the included program
+        let ast::Program {stmts: included_stmts} = expand_includes(
+            included_prog,
+            source_files,
+            diag,
+            depth-1,
+        );
+        // Even if this expansion ends with errors, we still want to include as much in the final
+        // result as we can, that's why we aren't checking `diag.emitted_errors()` here.
+
+        expanded_stmts.extend(included_stmts);
     }
 
-    diag.error("surpassed maximum include expansion attempts").emit();
-
-    ast::Program {stmts}
+    ast::Program {stmts: expanded_stmts}
 }
