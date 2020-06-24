1c2739f59eaf9d03e5ba142d8869c26a58caf4f0
Sunjay Varma <varma.sunjay@gmail.com>
2020-06-24 00:00:01 -0600

    Completed Reinterpret implementations for i128

--- vm/src/reinterpret/reinterpret_i128.rs
+++ vm/src/reinterpret/reinterpret_i128.rs
@@ -1,4 +1,17 @@
-use super::{Reinterpret, slice_16_as_8};
+use super::{
+    Reinterpret,
+    slice_16_as_8,
+    slice_16_as_4,
+    slice_16_as_2,
+    slice_16_as_1,
+};
+
+impl Reinterpret<i128> for u128 {
+    #[inline(always)]
+    fn reinterpret(value: i128) -> Self {
+        Self::from_le_bytes(value.to_le_bytes())
+    }
+}
 
 impl Reinterpret<i128> for u64 {
     #[inline(always)]
@@ -6,6 +19,76 @@ impl Reinterpret<i128> for u64 {
         // Reinterpret the lowest 8 bytes as u64
         let bytes = value.to_le_bytes();
         let bytes = slice_16_as_8(&bytes);
+        Self::from_le_bytes(*bytes)
+    }
+}
+
+impl Reinterpret<i128> for i64 {
+    #[inline(always)]
+    fn reinterpret(value: i128) -> Self {
+        // Reinterpret the lowest 8 bytes as i64
+        let bytes = value.to_le_bytes();
+        let bytes = slice_16_as_8(&bytes);
+        Self::from_le_bytes(*bytes)
+    }
+}
+
+impl Reinterpret<i128> for u32 {
+    #[inline(always)]
+    fn reinterpret(value: i128) -> Self {
+        // Reinterpret the lowest 4 bytes as u32
+        let bytes = value.to_le_bytes();
+        let bytes = slice_16_as_4(&bytes);
+        Self::from_le_bytes(*bytes)
+    }
+}
+
+impl Reinterpret<i128> for i32 {
+    #[inline(always)]
+    fn reinterpret(value: i128) -> Self {
+        // Reinterpret the lowest 4 bytes as i32
+        let bytes = value.to_le_bytes();
+        let bytes = slice_16_as_4(&bytes);
+        Self::from_le_bytes(*bytes)
+    }
+}
+
+impl Reinterpret<i128> for u16 {
+    #[inline(always)]
+    fn reinterpret(value: i128) -> Self {
+        // Reinterpret the lowest 2 bytes as u16
+        let bytes = value.to_le_bytes();
+        let bytes = slice_16_as_2(&bytes);
+        Self::from_le_bytes(*bytes)
+    }
+}
+
+impl Reinterpret<i128> for i16 {
+    #[inline(always)]
+    fn reinterpret(value: i128) -> Self {
+        // Reinterpret the lowest 2 bytes as i16
+        let bytes = value.to_le_bytes();
+        let bytes = slice_16_as_2(&bytes);
+        Self::from_le_bytes(*bytes)
+    }
+}
+
+impl Reinterpret<i128> for u8 {
+    #[inline(always)]
+    fn reinterpret(value: i128) -> Self {
+        // Reinterpret the lowest 1 byte as u8
+        let bytes = value.to_le_bytes();
+        let bytes = slice_16_as_1(&bytes);
+        Self::from_le_bytes(*bytes)
+    }
+}
+
+impl Reinterpret<i128> for i8 {
+    #[inline(always)]
+    fn reinterpret(value: i128) -> Self {
+        // Reinterpret the lowest 1 byte as i8
+        let bytes = value.to_le_bytes();
+        let bytes = slice_16_as_1(&bytes);
         Self::from_le_bytes(*bytes)
     }
 }
